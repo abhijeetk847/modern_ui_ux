4f9d9546b0f7b560aa9e21b49d7d7ea380ca562c
abhijeetk847 <abhijeetk847@outlook.com>
2022-02-23 17:23:46 +0530

    initial

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -10,15 +10,15 @@ const SpecialMenu = () => (
 		  <h1 className="headtext__cormorant">Today's Special</h1>
 	  </div>
 
-      <div className="app__specialMenu-menu">
-          <div className="app__specialMenu-menu_wine flex__center">
-              <p className="app__specialMenu_menu_heading">Wine & Beer</p>
-              <div className="app__specialMenu_menu_items">
-                  {data.wines.map((wine,index) => {
-                      return(<p>{wine.title}</p>)
-                  })}
-              </div>
-          </div>
+	  <div className="app__specialMenu-menu">
+		  <div className="app__specialMenu-menu_wine flex__center">
+			  <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+			  <div className="app__specialMenu_menu_items">
+				  {data.wines.map((wine,index) => {
+					  return(<p>{wine.title}</p>)
+				  })}
+			  </div>
+		  </div>
 
           <div className="app__specialMenu-menu_img">
               <img src={images.menu} alt="menu item"/>
